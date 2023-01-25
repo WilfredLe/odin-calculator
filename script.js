e64b7728f47cebdd3d2538e80c5693341aa6404b
Justin Le <mr.justintle@gmail.com>
2023-01-24 17:23:45 -0800

    Create btn function that changes display

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//Variables
+const mainDisplay = document.querySelector(".main-display");
+const secondaryDisplay = document.querySelector(".secondary-display");
+const testerBtn = document.querySelector("#tester-btn")
+const mainBtns = document.querySelectorAll(".main-btn");
+let mainDisContent = [];
+
+//Functions
 const calc = {
     add(x,y) {return x+y},
     subtract(x,y) {return x-y},
@@ -8,7 +16,24 @@ const calc = {
         else if(opertor === "-") {return this.subtract(x,y)}
         else if(opertor === "*") {return this.multiply(x,y)}
         else {return this.divide(x,y)}
-    }
+    },
+    btnFunc(elm) {
+        elm.addEventListener('click',
+            (e) => {
+                mainDisContent.push(`${e.target.innerText}`);
+                mainDisplay.innerText = mainDisContent.join('');
+                // mainDisplay.textContent = e.target.innerText;
+                // console.log(e.target.innerText)
+            }
+        )
+    },
 }
 
-console.log(calc.funcSele('*',2,3))
+// testerBtn.addEventListener(click,
+//     (e) => {
+//         // mainDisplay.textContent = this.
+//         console.log(e)
+//     }
+// )
+
+mainBtns.forEach((btn) => calc.btnFunc(btn))
